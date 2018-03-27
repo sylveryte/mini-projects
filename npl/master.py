731b8cae1f07ea1ef25b5cccb95b1d7f856426ca
sylveryte <sylveryte@gmail.com>
2018-03-28 01:01:14 +0530

    art of design :smirk:

--- npl/master.py
+++ npl/master.py
@@ -7,83 +7,172 @@ import sys
 
 import time
 
+import os
+
 
 class SlaveHandler:
-    def __init__(self, slave_socket, address, breakroom):
-        self.breakroom = breakroom
-        self.address = address
-        self.socket = slave_socket
-        self.dead = False
-        ready = self.socket.recv(1024).decode()
+    """
+    recieves  : 1 fail                        #no result
+                2 pass:[job]:[decrypted str]  #result
+                3 probe                       #probes when free [prober]
 
-        if ready.strip() == "ready":
-            threading.Thread(target=self.waiter).start()
+    responds  : 1 q                     #kill
+                2 rest                  #wait for instruction kill current task
+                3 job:[to_break]:[task] #new job for
+    """
+
+    def __init__(self, slave_socket, break_room, job_master, slave_id):
+        """
+
+        :type job_master: JobMaster
+        :type break_room: BreakRoom
+        """
+        self.slave_id = slave_id
+        self.job_master = job_master
+        self.break_room = break_room
+        self.socket = slave_socket
+        self.socket.setblocking(0)
+        self.socket.settimeout(1)
+        self.dead = False
+        threading.Thread(target=self.connection_manager).start()
+
+    def connection_manager(self):
+        waiter_thread = threading.Thread(target=self.waiter)
+        waiter_thread.start()
+        waiter_thread.join()
+        self.socket.close()
+        self.break_room.remove_slave_handler(self)
 
     def waiter(self):
-        self.send_new_job()
+        """
+        receiver + responder[jobs]
+        """
+        while True:
+            try:
+                if self.dead:
+                    break
+                reply = self.socket.recv(1024).decode()
+                if reply == '':
+                    self.dead = True
+                reply = reply.split(":")
+                # print("{} received {}".format(self.slave_id, reply))
+                if reply[0] == "probe":
+                    self.send_new_job()
+                elif reply[0] == "fail":
+                    pass
+                    # print("One fail")
+                elif reply[0] == "pass":
+                    self.break_room.got_result(reply[1] + ":" + reply[2])
+            except socket.timeout:
+                pass
+        print("Slave{} down ".format(self.slave_id))
+
+    def dispatcher(self):
+        """
+        responder[kill(q)]
+        """
         while True:
             if self.dead:
-                print("waiter down")
-                self.socket.send("rest".encode())
-                time.sleep(4)
-                self.socket.close()
+                self.socket.send("q".encode())
                 break
-            result = self.socket.recv(1024).decode()
-            if result is ":":
-                self.send_new_job()
-                continue
-            elif result:
-                self.breakroom.got_result(result.encode())
+            time.sleep(3)
 
     def send_new_job(self):
-        print("sending new job to {}".format(self.address))
-        self.socket.send(self.breakroom.get_next_task().encode())
+        self.socket.send(self.job_master.get_task().encode())
 
     def close_connection(self):
         self.dead = True
+
+    def send_rest(self):
         self.socket.send("rest".encode())
 
 
-class BreakRoom:
-    def __init__(self, hash_to_break, port,host):
-        self.result = ""
-        self.hash_to_break = hash_to_break
+class JobMaster:
+    def __init__(self, sha1code, break_room):
+        """
 
+        :type break_room: BreakRoom
+        """
+        self.break_room = break_room
+        self.the_queue = Queue()
+        self.currentJob = None
+        self.from_file = False
+        self.jobs = []
+        if os.path.isfile(sha1code):
+            self.from_file = True
+            self.result_file = open("decrypted" + sha1code + ".txt", 'w')
+            print("Decrypting from file. Results in Decrypted+{}".format(sha1code))
+            for x in open(sha1code, 'r').read().split():
+                self.jobs.append(x)
+        else:
+            self.jobs.append(sha1code)
+
+        # print("JOB MASTER'S JOBS : {}".format(self.jobs))
+        self._new_job()
+
+    def _generate_new_queue(self):
         nums = '1234567890'
         special = '`~!@#$%^&*()_+-='
         space = ' '
+        self.the_queue = Queue()
+        self.the_queue.put(str(string.ascii_lowercase))
+        self.the_queue.put(str(string.ascii_letters))
+        self.the_queue.put(str(string.ascii_lowercase + nums))
+        self.the_queue.put(str(string.ascii_letters + nums))
+        self.the_queue.put(str(string.ascii_uppercase))
+        self.the_queue.put(str(string.ascii_uppercase + nums))
+        self.the_queue.put(str(string.ascii_lowercase + nums + special))
+        self.the_queue.put(str(string.ascii_lowercase + special))
+        self.the_queue.put(str(string.ascii_letters + special))
+        self.the_queue.put(str(string.ascii_letters + nums + special))
+        self.the_queue.put(str(string.ascii_uppercase + nums + special))
+        self.the_queue.put(str(string.ascii_uppercase + special))
+        self.the_queue.put(str(string.ascii_lowercase + nums + space))
+        self.the_queue.put(str(string.ascii_uppercase + nums + space))
+        self.the_queue.put(str(string.ascii_letters + nums + space))
+        self.the_queue.put(str(string.ascii_lowercase + nums + space))
+        self.the_queue.put(str(string.ascii_uppercase + nums + space))
+        self.the_queue.put(str(string.ascii_letters + nums + space))
 
-        self.result_received = False
+    def _new_job(self):
+        if len(self.jobs) > 0:
+            self.currentJob = self.jobs.pop()
+            self._generate_new_queue()
+            print("Decrypting : {}".format(self.currentJob))
+        elif len(self.jobs) == 0:
+            self.currentJob = None
+            self.break_room.we_are_done()
 
-        self.thequeue = Queue()
-        self.thequeue.put(str(string.ascii_lowercase))
-        self.thequeue.put(str(string.ascii_letters))
-        self.thequeue.put(str(string.ascii_lowercase + nums))
-        self.thequeue.put(str(string.ascii_letters + nums))
-        self.thequeue.put(str(string.ascii_uppercase))
-        self.thequeue.put(str(string.ascii_uppercase + nums))
-        self.thequeue.put(str(string.ascii_lowercase + nums + special))
-        self.thequeue.put(str(string.ascii_lowercase + special))
-        self.thequeue.put(str(string.ascii_letters + special))
-        self.thequeue.put(str(string.ascii_letters + nums + special))
-        self.thequeue.put(str(string.ascii_uppercase + nums + special))
-        self.thequeue.put(str(string.ascii_uppercase + special))
-        self.thequeue.put(str(string.ascii_lowercase + nums + space))
-        self.thequeue.put(str(string.ascii_uppercase + nums + space))
-        self.thequeue.put(str(string.ascii_letters + nums + space))
-        self.thequeue.put(str(string.ascii_lowercase + nums + space))
-        self.thequeue.put(str(string.ascii_uppercase + nums + space))
-        self.thequeue.put(str(string.ascii_letters + nums + space))
+    def get_task(self):
+        if not self.currentJob:
+            temp_str = "q"
+        elif self.the_queue.empty():
+            self._new_job()
+            temp_str = "rest"
+        else:
+            temp_str = "job:" + self.currentJob + ':' + self.the_queue.get()
+        # print("JOb master : {}".format(temp_str))
+        return temp_str
 
-        print("Decrypting : {}".format(hash_to_break))
+    def take_result(self, result):
+        result = result.split(":")
+        print("########### {} -> {}".format(result[0], result[1]))
+        self.break_room.send_rest()
+        self._new_job()
+        if self.from_file:
+            self.result_file.write(result[0] + " -> " + result[1] + '\n')
 
-        self.slave_sockets = []
+
+class BreakRoom:
+    def __init__(self, host, port, sha1code):
+        self.failure = 0
+        self.we_are_done_switch = False
+        self.jobMaster = JobMaster(sha1code, self)
+
+        self.slave_handlers = []
 
         self.master_socket = socket.socket()
-        self.host = host
-        print(self.host)
-        self.port = port
-        self.master_socket.bind((self.host, self.port))
+        self.master_socket.bind((host, port))
 
         threading.Thread(name="receiptionist", target=self.receive_incoming_request).start()
 
@@ -91,34 +180,44 @@ class BreakRoom:
         self.master_socket.listen(20)
         self.master_socket.setblocking(0)
         self.master_socket.settimeout(2)
+        slave_id = 0
         while True:
             try:
                 slave_socket, addr = self.master_socket.accept()
-                slave_socket.send(self.hash_to_break.encode())
                 print('Got connection from {}'.format(addr))
-                self.slave_sockets.append(SlaveHandler(slave_socket, addr, self))
+                self.slave_handlers.append(SlaveHandler(slave_socket, self, self.jobMaster, slave_id))
+                slave_id += 1
             except socket.timeout:
-                if self.result_received:
+                if self.we_are_done_switch and not len(self.slave_handlers):
+                    self._close_all_connections()
                     break
         print("Receiptionist down.")
-        print("Password is '{}'".format(self.result))
 
-    def get_next_task(self):
-        if self.thequeue.empty() or self.result_received:
-            return "rest"
-        else:
-            return self.thequeue.get()
-
-    def close_all_connections(self):
-        for slave in self.slave_sockets:
+    def _close_all_connections(self):
+        for slave in self.slave_handlers:
             slave.close_connection()
-        print("Slaves closed")
+        # print("Slaves closed")
 
     def got_result(self, result):
-        self.result_received = True
-        self.result = result.decode()
-        self.close_all_connections()
+        self.jobMaster.take_result(result)
+
+    def we_are_done(self):
+        self.we_are_done_switch = True
+        # print("WE ARE DONE!!")
+
+    def remove_slave_handler(self, slave_handler):
+        self.slave_handlers.pop(self.slave_handlers.index(slave_handler))
+
+    def send_rest(self):
+        for x in self.slave_handlers:
+            x.send_rest()
 
 
 if __name__ == '__main__':
-    BreakRoom(str(sys.argv[3]), int(sys.argv[2]),sys.argv[1])
+    pass
+    # print(sys.argv)
+    if len(sys.argv) == 4:
+        BreakRoom(sys.argv[1], int(sys.argv[2]), sys.argv[3])
+    else:
+        print("syntax : python3 master.py [local ip addr] [port] [sha1 code /sha1 file] optional[no of localslaves]\n "
+              "eg     : python3 master.py 192.168.1.105 9987 e5acb1a96e34cd7f263aada0b69aa58bdd722a03 3 ")
--- npl/slave.py
+++ npl/slave.py
@@ -18,78 +18,124 @@ def get_sha1(s):
 
 
 class Slave:
+    """
+    receives  : 1 q                     #kill
+                2 rest                  #wait for instruction kill current task
+                3 job:[to_break]:[task] #new job for
+
+    responds  : 1 fail                        #no result
+                2 pass:[job]:[decrypted str]  #result
+                3 probe                       #probes when free [prober]
+    """
+
     def __init__(self, host, port):
+        self.limit = 2
         self.slave_socket = socket.socket()
         print("Connecting to {} on port {}".format(host, port))
 
         self.dead = False
-        self.killer_on = False
+        self.busy = False
+        self.stop = True
+        self.to_break = ""
+        self.task = ""
 
         self.slave_socket.connect((host, port))
-        self.to_break = self.slave_socket.recv(1024).decode()
         self.slave_socket.setblocking(0)
         self.slave_socket.settimeout(1)
-        print("to break : {}".format(self.to_break))
+        threading.Thread(target=self.connection_manager).start()
 
-        self.slave_socket.send("ready".encode())
+    def connection_manager(self):
+        waiter_thread = threading.Thread(target=self.waiter)
+        executioner_thread = threading.Thread(target=self.executioner)
+        prober_thread = threading.Thread(target=self.prober)
+        waiter_thread.start()
+        executioner_thread.start()
+        prober_thread.start()
+        prober_thread.join()
+        waiter_thread.join()
+        executioner_thread.join()
+        self.slave_socket.close()
 
-        threading.Thread(target=self.execution).start()
+    def prober(self):
+        """
+        probes master when free by sending 'probe'
+        :return:
+        """
+        # print("prober in")
+        while True:
+            time.sleep(3)
+            if self.dead:
+                break
+            if not self.busy:
+                self.slave_socket.send("probe".encode())
+                self.busy = True
+                # print("probed!")
 
-    def smart_killer(self):
+    def waiter(self):
+        """
+        receiver
+        """
         while True:
             try:
-                job_str = self.slave_socket.recv(1024).decode()
-                if job_str.strip() == "rest":
-                    self.dead = True
+                if self.dead:
                     break
+                reply = self.slave_socket.recv(1024).decode().strip().split(":")
+                if reply[0] == '':
+                    self.dead = True
+                if reply[0] == "q":
+                    self.dead = True
+                    self.stop = True
+                    break
+                if reply[0] == "rest":
+                    self.stop = True
+                    self.busy = False
+                    # print("rest received..................")
+                if reply[0] == "job":
+                    self.stop = False
+                    self.to_break = reply[1]
+                    self.task = reply[2]
+                    print("New job : {} task : {}".format(self.to_break, self.task))
+                # print(reply)
             except socket.timeout:
                 pass
-                # print("smartkiller missed")
+        print("Receiver down")
 
-        print("smart killer killed again")
-        time.sleep(3)
-        self.slave_socket.close()
-        print("Slave closed")
-
-    def execution(self):
+    def executioner(self):
+        """
+        responder
+        """
         while True:
             if self.dead:
                 break
-            try:
-                job_str = self.slave_socket.recv(1024).decode()
-                job_str = job_str.strip()
-                if job_str == "rest":
-                    break
-                print("job is : {}".format(job_str))
-                if not self.killer_on:
-                    threading.Thread(target=self.smart_killer).start()
-                    self.killer_on = True
+            if self.stop:
+                pass
+            else:
+                self.busy = True
+                # print("BREAKING {} with {}".format(self.to_break, self.task))
+                response = self.break_code()
+                self.busy = False
+                print("RESPONSE ({}) for BREAKING {} with {}".format(response, self.to_break, self.task))
+                if response:
+                    self.slave_socket.send(response.rstrip().encode())
+            time.sleep(3)
+        print("Receiver down")
 
-                result = self.break_code(job_str)
-                print("breaking result : {}".format(result))
-                if result:
-                    print("Sending result ({})".format(result))
-                    self.slave_socket.send(result.encode())
-                else:
-                    print("Requesting for new job!! by sending : ")
-                    self.slave_socket.send(":".encode())
-            except socket.timeout:
-                print("Executioner xpd a timeout")
-        print("executioner down")
-
-    def break_code(self, char_set, length=1):
-        print("Entering with {}|{}".format(length, char_set))
-        for g in brute_force(char_set, length):
-            if self.dead:
-                return
-            if get_sha1(g) == self.to_break:
-                return g
-        if length > 5:
-            print("LENGHT LIMIT REACHED {}".format(length))
-            return None
+    def break_code(self, length=1):
+        """
+        just a tool of slave
+        """
+        print("/")
+        for guess in brute_force(self.task, length):
+            if self.stop:
+                return None
+            if get_sha1(guess) == self.to_break:
+                return "pass:" + self.to_break + ':' + guess
+        if length > self.limit:
+            # print("LENGTH LIMIT REACHED {}".format(length))
+            return "fail"
         else:
-            return self.break_code(char_set, length + 1)
+            return self.break_code(length + 1)
 
 
 if __name__ == '__main__':
-    Slave(sys.argv[1],int(sys.argv[2]))
+    Slave(sys.argv[1], int(sys.argv[2]))
